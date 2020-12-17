fec9bb78bc84e845e73d36cddd8ea67b0529b35f
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-12-17 01:12:03 -0300

    adding initial routes,

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route,
+} from 'react-router-dom';
 import Home from './pages/Home';
+import LoginRegister from './pages/LoginRegister';
+import Settings from './pages/Settings';
+import CreateEditArticle from './pages/CreateEditArticle';
+import Article from './pages/Article';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Switch>
+        <Route path={['/login', '/register']}>
+          <LoginRegister />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path={['/editor', '/editor/:slug']}>
+          <CreateEditArticle />
+        </Route>
+        <Route path="/article/:slug">
+          <Article />
+        </Route>
+        <Route path={['/profile/:slug', '/profile/:slug/favorites']}>
+          <Profile />
+        </Route>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
