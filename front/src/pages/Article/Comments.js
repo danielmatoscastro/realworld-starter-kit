0ce3bec1c271fbec5b59cef1c2434cc5508e0cda
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-01-04 23:29:19 -0300

    fix: fix broken images

--- front/src/pages/Article/Comments.js
+++ front/src/pages/Article/Comments.js
@@ -11,7 +11,7 @@ const Comments = ({ comments }) => (
       </div>
       <div className="card-footer">
         <Link to={PROFILE_F(comment.author.username)} className="comment-author">
-          <img src={comment.author.username} className="comment-author-img" alt="comment author" />
+          <img src={comment.author.image} className="comment-author-img" alt="comment author" />
         </Link>
 &nbsp;
         <Link to={PROFILE_F(comment.author.username)} className="comment-author">{comment.author.username}</Link>
