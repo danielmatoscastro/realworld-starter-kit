eae477ca5e83ed18966bb7c0a1a4d40fcf8cf2b6
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-01-17 22:00:34 -0300

    perf: use React.lazy for bundle optimization (code-splitting)

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
@@ -14,46 +14,47 @@ import {
   HOME,
 } from './routes';
 import { UserProvider } from './providers';
-import {
-  Home,
-  Login,
-  Register,
-  Settings,
-  CreateArticle,
-  Article,
-  Profile,
-  EditArticle,
-} from './pages';
+
+const Home = React.lazy(() => import('pages/Home'));
+const Login = React.lazy(() => import('pages/LoginRegister'));
+const Register = React.lazy(() => import('pages/LoginRegister'));
+const Settings = React.lazy(() => import('pages/Settings'));
+const CreateArticle = React.lazy(() => import('pages/CreateArticle'));
+const EditArticle = React.lazy(() => import('pages/EditArticle'));
+const Article = React.lazy(() => import('pages/Article'));
+const Profile = React.lazy(() => import('pages/Profile'));
 
 function App() {
   return (
     <UserProvider>
       <Router>
         <Switch>
-          <Route path={LOGIN}>
-            <Login />
-          </Route>
-          <Route path={REGISTER}>
-            <Register />
-          </Route>
-          <Route path={SETTINGS}>
-            <Settings />
-          </Route>
-          <Route path={EDITOR} exact>
-            <CreateArticle />
-          </Route>
-          <Route path={EDITOR_SLUG}>
-            <EditArticle />
-          </Route>
-          <Route path={ARTICLE_SLUG}>
-            <Article />
-          </Route>
-          <Route path={[PROFILE_SLUG, FAVORITES_SLUG]}>
-            <Profile />
-          </Route>
-          <Route path={HOME} exact>
-            <Home />
-          </Route>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Route path={LOGIN}>
+              <Login />
+            </Route>
+            <Route path={REGISTER}>
+              <Register />
+            </Route>
+            <Route path={SETTINGS}>
+              <Settings />
+            </Route>
+            <Route path={EDITOR} exact>
+              <CreateArticle />
+            </Route>
+            <Route path={EDITOR_SLUG}>
+              <EditArticle />
+            </Route>
+            <Route path={ARTICLE_SLUG}>
+              <Article />
+            </Route>
+            <Route path={[PROFILE_SLUG, FAVORITES_SLUG]}>
+              <Profile />
+            </Route>
+            <Route path={HOME} exact>
+              <Home />
+            </Route>
+          </Suspense>
         </Switch>
       </Router>
     </UserProvider>
